bbbf2113abd7c3d6a3cc038da6487b24a6bfe18a
guisabinou <gui.sabinof@gmail.com>
2023-05-30 21:38:51 -0300

    adicionado funçoes em arquivos ts

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json"
 
 @Component({
   selector: 'app-quiz',
@@ -11,9 +12,26 @@ export class QuizComponent {
   questions:any
   questionSelected:any
 
+  answers:string[] = []
+  answerSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+    if(quiz_questions) {
+      this.finished = false
+      this.title = quiz_questions.title
 
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
   }
 }
