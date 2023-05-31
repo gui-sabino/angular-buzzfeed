d6f09cfd6a6cbc6ee8fd798da2c721de65aadd67
guisabinou <gui.sabinof@gmail.com>
2023-05-30 21:25:40 -0300

    projeto iniciado

---
+++ src/app/components/quiz/quiz.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+  styleUrls: ['./quiz.component.css']
+})
+export class QuizComponent {
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+}
