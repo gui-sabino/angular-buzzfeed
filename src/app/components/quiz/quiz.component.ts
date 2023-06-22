2a4d2886685f54b849279b223ddff389caa499a4
Gui Sabino <112764907+gui-sabino@users.noreply.github.com>
2023-06-22 11:45:42 -0300

    finalizando a parte logica

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -37,15 +37,31 @@ export class QuizComponent {
 
   playerChoose(value: string) {
     this.answers.push(value)
+    this.nextStep()
   }
 
   async nextStep() {
     this.questionIndex += 1
 
-    if(this.questionMaxIndex > this.questionIndex){
+    if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex]
     } else {
+      const finalAnswer: string = await this.checkResult(this.answers)
       this.finished = true
+      this.answerSelected = quiz_questions.results[finalAnswer as 
+        keyof typeof quiz_questions.results]
     }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if (arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length) {
+        return previous
+      } else {
+        return current
+      }
+    })
+    return result
   }
 }
